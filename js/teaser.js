c803be8e2f7825f311ff4504216441b50b98ccc8
Agnieszka <jagnaapps@gmail.com>
2015-06-12 11:56:50 +0200

    initial

---
+++ js/teaser.js
@@ -0,0 +1,12 @@
+﻿var Teaser = (function ()
+{
+    var init = function($elem) {
+        $elem.append('<div class="teaser__icon"></div>');
+        $elem.prepend('<div class="teaser__icon"></div>');
+    }
+
+    return {
+        init: init
+    };
+
+}());
---
+++ js/teaserManager.js
@@ -0,0 +1,13 @@
+﻿var TeaserManager = (function () {
+
+    var initAll = function() {
+        $(".js-teaser").each(function() {
+            Teaser.init($(this));
+        });
+    }
+
+    return {
+        initAll: initAll
+    };
+
+}());
